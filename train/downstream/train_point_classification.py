5b091ffbb178f556c1bee2ba1f36e94c5b1d998b
henriettahall31-prog <henrietta.hall31@gmail.com>
2026-08-29 08:56:24 -0500

    Update train_point_classification.py
    checkpoint resume

--- train/downstream/train_point_classification.py
+++ train/downstream/train_point_classification.py
@@ -16,4 +16,12 @@ if __name__ == '__main__':
     params = YParams(os.path.abspath(args.yaml_config), args.config)
     trainer = DownstreamTrainer(params, args)
     trainer.launch()
+
+#train from checkpoints
+    import os
+checkpoint_path = os.path.join('/scratch/midway3/hhall5/checkpoints/pid_microboone', 'pid_training_checkpoint.pth')
+if os.path.exists(checkpoint_path):
+    print(f"Resuming from {checkpoint_path}")
+    trainer.train(pretrain=True, train_from_checkpoint=True, checkpoint_path=checkpoint_path)
+else:
     trainer.train(pretrain=True)
